cf960908b2250a48f0ca87181d6f8557eb475fb7
junqingchang <jqchang95@gmail.com>
2020-04-24 09:12:31 +0800

    Merging Lin parts

--- dataprocessing.py
+++ dataprocessing.py
@@ -321,3 +321,71 @@ class BERTMNLIWithWordMaskingOwnTokenizer(Dataset):
                     data.append(loaded_example)
             random.shuffle(data)
         return data
+
+
+class BERTMNLIWithWordMaskingAndLossEdit(Dataset):
+    def __init__(self, path, genre=False, snli=True, batch_size=8, bert_type='bert-base-cased'):
+        self.LABEL_MAP = {
+            "entailment": 0,
+            "neutral": 1,
+            "contradiction": 2,
+            "hidden": 0
+        }
+        self.genre = genre
+        self.snli = snli
+        self.dataset = self.load_nli_data_genre(path, genre, snli)
+        self.tokenizer = BertTokenizerWithWordMasking.from_pretrained(bert_type)
+        self.batch_size = batch_size
+
+    def __getitem__(self, idx):
+        if idx < self.__len__():
+            sentence1 = [[x['sentence1'], x['sentence2']]
+                        for x in self.dataset[self.batch_size*idx:self.batch_size*idx+self.batch_size]]
+            sentence2 = [[x['sentence2'], x['sentence1']]
+                        for x in self.dataset[self.batch_size*idx:self.batch_size*idx+self.batch_size]]
+            label = [x['label'] for x in self.dataset[self.batch_size *
+                                                      idx:self.batch_size*idx+self.batch_size]]
+        else:
+            sentence1 = [[x['sentence1'], x['sentence2']]
+                        for x in self.dataset[self.batch_size*idx:]]
+            sentence2 = [[x['sentence2'], x['sentence1']]
+                        for x in self.dataset[self.batch_size*idx:]]
+            label = [x['label'] for x in self.dataset[self.batch_size*idx:]]
+
+        sentence1 = self.tokenizer.batch_encode_plus(
+            sentence1, pad_to_max_length=True, return_token_type_ids=True, return_attention_masks=True)
+        for key in sentence1:
+            sentence1[key] = torch.tensor(sentence1[key])
+        sentence2 = self.tokenizer.batch_encode_plus(
+            sentence2, pad_to_max_length=True, return_token_type_ids=True, return_attention_masks=True)
+        for key in sentence2:
+            sentence2[key] = torch.tensor(sentence2[key])
+
+        return [sentence1, sentence2], torch.tensor(label)
+
+    def __len__(self):
+        return math.ceil(len(self.dataset)/self.batch_size)
+
+    def load_nli_data_genre(self, path, genre, snli=True):
+        """
+        Taken from official MNLI github
+
+        Load a specific genre's examples from MultiNLI, or load SNLI data and assign a "snli" genre to the examples.
+        If the "snli" parameter is set to True, a genre label of snli will be assigned to the data. If set to true, it will overwrite the genre label for MultiNLI data.
+        """
+        data = []
+        with open(path) as f:
+            for line in f:
+                loaded_example = json.loads(line)
+                if loaded_example["gold_label"] not in self.LABEL_MAP:
+                    continue
+                loaded_example["label"] = self.LABEL_MAP[loaded_example["gold_label"]]
+                if snli:
+                    loaded_example["genre"] = "snli"
+                if genre:
+                    if loaded_example["genre"] == genre:
+                        data.append(loaded_example)
+                else:
+                    data.append(loaded_example)
+            random.shuffle(data)
+        return data
--- model.py
+++ model.py
@@ -498,3 +498,269 @@ class BERTWithWordMaskingSelfPretrained(nn.Module):
         logits = self.classifier(pooled_output)
 
         return logits
+
+#-----------------------------modify pooler layer activation ------------------------
+class BertPooler_Sigmoid(nn.Module):
+    def __init__(self, config):
+        super(BertPooler_Sigmoid, self).__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        #self.activation = nn.Tanh() #--original
+        self.activation = nn.Sigmoid()
+
+    def forward(self, hidden_states):
+        # We "pool" the model by simply taking the hidden state corresponding
+        # to the first token.
+        first_token_tensor = hidden_states[:, 0]
+        pooled_output = self.dense(first_token_tensor)
+        pooled_output = self.activation(pooled_output)
+        return pooled_output
+    
+class BertPooler_reLu(nn.Module):
+    def __init__(self, config):
+        super(BertPooler_reLu, self).__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        #self.activation = nn.Tanh() #--original
+        self.activation = nn.ReLu()
+
+    def forward(self, hidden_states):
+        # We "pool" the model by simply taking the hidden state corresponding
+        # to the first token.
+        first_token_tensor = hidden_states[:, 0]
+        pooled_output = self.dense(first_token_tensor)
+        pooled_output = self.activation(pooled_output)
+        return pooled_output
+
+
+class BertModel_PoolActivation(BertPreTrainedModel):
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.embeddings = BertEmbeddings(config)
+        self.encoder = BertEncoder(config)
+        #self.pooler = BertPooler(config)
+        self.pooler =  BertPooler_Sigmoid(config)
+        #self.pooler = BertPooler_reLu(config)
+
+        #self.apply(self.init_weights)
+        self.init_weights()
+    def _resize_token_embeddings(self, new_num_tokens):
+        old_embeddings = self.embeddings.word_embeddings
+        new_embeddings = self._get_resized_embeddings(old_embeddings, new_num_tokens)
+        self.embeddings.word_embeddings = new_embeddings
+        return self.embeddings.word_embeddings
+
+    def _prune_heads(self, heads_to_prune):
+        for layer, heads in heads_to_prune.items():
+            self.encoder.layer[layer].attention.prune_heads(heads)
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, position_ids=None, head_mask=None):
+        if attention_mask is None:
+            attention_mask = torch.ones_like(input_ids)
+        if token_type_ids is None:
+            token_type_ids = torch.zeros_like(input_ids)
+
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype) # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+
+        if head_mask is not None:
+            if head_mask.dim() == 1:
+                head_mask = head_mask.unsqueeze(0).unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+                head_mask = head_mask.expand(self.config.num_hidden_layers, -1, -1, -1, -1)
+            elif head_mask.dim() == 2:
+                head_mask = head_mask.unsqueeze(1).unsqueeze(-1).unsqueeze(-1)  # We can specify head_mask for each layer
+            head_mask = head_mask.to(dtype=next(self.parameters()).dtype) # switch to fload if need + fp16 compatibility
+        else:
+            head_mask = [None] * self.config.num_hidden_layers
+
+        embedding_output = self.embeddings(input_ids, position_ids=position_ids, token_type_ids=token_type_ids)
+        encoder_outputs = self.encoder(embedding_output, extended_attention_mask,head_mask=head_mask)
+        sequence_output = encoder_outputs[0]
+        pooled_output = self.pooler(sequence_output)
+
+        outputs = (sequence_output, pooled_output,) + encoder_outputs[1:]  # add hidden_states and attentions if they are here
+        return outputs  # sequence_output, pooled_output, (hidden_states), (attentions)
+
+#-------------------------for pool layer activation function--------
+class BERT_withPoolActivation(nn.Module):
+    def __init__(self, num_classes=3, bert_type='bert-large-cased'):
+        super(BERT_withPoolActivation, self).__init__()
+        assert bert_type == 'bert-large-cased' or bert_type == 'bert-base-cased'
+        
+        self.bert = BertModel_PoolActivation.from_pretrained(bert_type)
+        self.dropout = nn.Dropout(0.1)
+        if bert_type == 'bert-base-cased':
+            self.classifier = nn.Linear(768, num_classes)
+        elif bert_type == 'bert-large-cased':
+            self.classifier = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        outputs = self.bert(x['input_ids'], attention_mask=x['attention_mask'], token_type_ids=x['token_type_ids'])
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        return logits
+
+
+class BertWithAllEdits(BertPreTrainedModel):
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
+
+        self.embeddings = BertEmbeddingsWithWordMasking(config)
+        self.encoder = BertEncoder(config)
+        self.pooler = BertPooler_Sigmoid(config)
+
+        self.init_weights()
+
+    def get_input_embeddings(self):
+        return self.embeddings.word_embeddings
+
+
+    def set_input_embeddings(self, value):
+        self.embeddings.word_embeddings = value
+
+
+    def _prune_heads(self, heads_to_prune):
+        """ Prunes heads of the model.
+            heads_to_prune: dict of {layer_num: list of heads to prune in this layer}
+            See base class PreTrainedModel
+        """
+        for layer, heads in heads_to_prune.items():
+            self.encoder.layer[layer].attention.prune_heads(heads)
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        encoder_hidden_states=None,
+        encoder_attention_mask=None,
+        word_mask=None
+    ):
+
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        elif input_ids is not None:
+            input_shape = input_ids.size()
+        elif inputs_embeds is not None:
+            input_shape = inputs_embeds.size()[:-1]
+        else:
+            raise ValueError("You have to specify either input_ids or inputs_embeds")
+
+        device = input_ids.device if input_ids is not None else inputs_embeds.device
+
+        if attention_mask is None:
+            attention_mask = torch.ones(input_shape, device=device)
+        if token_type_ids is None:
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+        if word_mask is None:
+            word_mask = torch.zeros(input_shape, dtype=torch.long, device=device)
+
+        if attention_mask.dim() == 3:
+            extended_attention_mask = attention_mask[:, None, :, :]
+        elif attention_mask.dim() == 2:
+            if self.config.is_decoder:
+                batch_size, seq_length = input_shape
+                seq_ids = torch.arange(seq_length, device=device)
+                causal_mask = seq_ids[None, None, :].repeat(batch_size, seq_length, 1) <= seq_ids[None, :, None]
+                causal_mask = causal_mask.to(
+                    attention_mask.dtype
+                )  # causal and attention masks must have same type with pytorch version < 1.3
+                extended_attention_mask = causal_mask[:, None, :, :] * attention_mask[:, None, None, :]
+            else:
+                extended_attention_mask = attention_mask[:, None, None, :]
+        else:
+            raise ValueError(
+                "Wrong shape for input_ids (shape {}) or attention_mask (shape {})".format(
+                    input_shape, attention_mask.shape
+                )
+            )
+
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+        if self.config.is_decoder and encoder_hidden_states is not None:
+            encoder_batch_size, encoder_sequence_length, _ = encoder_hidden_states.size()
+            encoder_hidden_shape = (encoder_batch_size, encoder_sequence_length)
+            if encoder_attention_mask is None:
+                encoder_attention_mask = torch.ones(encoder_hidden_shape, device=device)
+
+            if encoder_attention_mask.dim() == 3:
+                encoder_extended_attention_mask = encoder_attention_mask[:, None, :, :]
+            elif encoder_attention_mask.dim() == 2:
+                encoder_extended_attention_mask = encoder_attention_mask[:, None, None, :]
+            else:
+                raise ValueError(
+                    "Wrong shape for encoder_hidden_shape (shape {}) or encoder_attention_mask (shape {})".format(
+                        encoder_hidden_shape, encoder_attention_mask.shape
+                    )
+                )
+
+            encoder_extended_attention_mask = encoder_extended_attention_mask.to(
+                dtype=next(self.parameters()).dtype
+            )  # fp16 compatibility
+            encoder_extended_attention_mask = (1.0 - encoder_extended_attention_mask) * -10000.0
+        else:
+            encoder_extended_attention_mask = None
+
+        if head_mask is not None:
+            if head_mask.dim() == 1:
+                head_mask = head_mask.unsqueeze(0).unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+                head_mask = head_mask.expand(self.config.num_hidden_layers, -1, -1, -1, -1)
+            elif head_mask.dim() == 2:
+                head_mask = (
+                    head_mask.unsqueeze(1).unsqueeze(-1).unsqueeze(-1)
+                )  # We can specify head_mask for each layer
+            head_mask = head_mask.to(
+                dtype=next(self.parameters()).dtype
+            )  # switch to fload if need + fp16 compatibility
+        else:
+            head_mask = [None] * self.config.num_hidden_layers
+
+        embedding_output = self.embeddings(
+            input_ids=input_ids, position_ids=position_ids, token_type_ids=token_type_ids, inputs_embeds=inputs_embeds, word_mask=word_mask
+        )
+        encoder_outputs = self.encoder(
+            embedding_output,
+            attention_mask=extended_attention_mask,
+            head_mask=head_mask,
+            encoder_hidden_states=encoder_hidden_states,
+            encoder_attention_mask=encoder_extended_attention_mask,
+        )
+        sequence_output = encoder_outputs[0]
+        pooled_output = self.pooler(sequence_output)
+
+        outputs = (sequence_output, pooled_output,) + encoder_outputs[
+            1:
+        ]  # add hidden_states and attentions if they are here
+        return outputs  # sequence_output, pooled_output, (hidden_states), (attentions)
+
+
+class BERTAllInOne(nn.Module):
+    def __init__(self, num_classes=3, bert_type='bert-base-cased'):
+        super(BERTAllInOne, self).__init__()
+        assert bert_type == 'bert-large-cased' or bert_type == 'bert-base-cased'
+        
+        self.bert = BertWithAllEdits.from_pretrained(bert_type)
+        self.dropout = nn.Dropout(0.1)
+        if bert_type == 'bert-base-cased':
+            self.classifier = nn.Linear(768, num_classes)
+        elif bert_type == 'bert-large-cased':
+            self.classifier = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        outputs = self.bert(x['input_ids'], attention_mask=x['attention_mask'], token_type_ids=x['token_type_ids'])
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        return logits

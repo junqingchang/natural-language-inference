09b0c70782e3b9e761a329b195d778648e1cf770
cathylinxi <58717293+cathylinxi@users.noreply.github.com>
2020-05-04 01:01:59 +0800

    add for pre-fintuning

--- dataprocessing.py
+++ dataprocessing.py
@@ -107,6 +107,70 @@ class BERTMNLI(Dataset):
             random.shuffle(data)
         return data
 
+class BERTMNLI_preFinetuning(Dataset):
+    def __init__(self, data,  genre=False, snli=True, batch_size=8, bert_type='bert-base-cased'):
+        self.LABEL_MAP = {
+            "entailment": 0,
+            "neutral": 1,
+            "contradiction": 2,
+            "hidden": 0
+        }
+        self.genre = genre
+        self.snli = snli
+        self.dataset = data
+        self.tokenizer = BertTokenizer.from_pretrained(bert_type)
+        self.batch_size = batch_size
+
+    def __getitem__(self, idx):
+        if idx < self.__len__():
+            print(f' {self.batch_size*idx}')
+            print(f' {self.batch_size * idx + self.batch_size}')
+            
+            sentence = [[x[0], x[1]]
+                        for x in self.dataset[self.batch_size*idx:self.batch_size*idx+self.batch_size]]
+            label = [x[2] for x in self.dataset[self.batch_size *
+                                                      idx:self.batch_size*idx+self.batch_size]]
+            #print(f' {sentence}')
+            print(f' {label}')
+        else:
+            sentence = [[x[0], x[1]]
+                        for x in self.dataset[self.batch_size*idx:]]
+            label = [x[2] for x in self.dataset[self.batch_size*idx:]]
+        
+        sentence = self.tokenizer.batch_encode_plus(
+            sentence, pad_to_max_length=True, return_token_type_ids=True, return_attention_masks=True)
+        
+        for key in sentence:
+            sentence[key] = torch.tensor(sentence[key])
+        return sentence, torch.tensor(label)
+        #return sentence
+
+    def __len__(self):
+        return math.ceil(len(self.dataset)/self.batch_size)
+
+    def load_nli_data_genre(self, path, genre, snli=True):
+        """
+        Taken from official MNLI github
+
+        Load a specific genre's examples from MultiNLI, or load SNLI data and assign a "snli" genre to the examples.
+        If the "snli" parameter is set to True, a genre label of snli will be assigned to the data. If set to true, it will overwrite the genre label for MultiNLI data.
+        """
+        data = []
+        with open(path) as f:
+            for line in f:
+                loaded_example = json.loads(line)
+                if loaded_example["gold_label"] not in self.LABEL_MAP:
+                    continue
+                loaded_example["label"] = self.LABEL_MAP[loaded_example["gold_label"]]
+                if snli:
+                    loaded_example["genre"] = "snli"
+                if genre:
+                    if loaded_example["genre"] == genre:
+                        data.append(loaded_example)
+                else:
+                    data.append(loaded_example)
+            random.shuffle(data)
+        return data
 
 class LossEditedBERTMNLI(Dataset):
     def __init__(self, path, genre=False, snli=True, batch_size=8, bert_type='bert-base-cased'):
